a6ce47f5a18dfd155ab27d4b990b64ed308e238e
EmanuelBatixta <emanueljonatas28@gmail.com>
2025-01-14 21:44:46 -0300

    solved w1

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartList = getLocalStorage("so-cart");
+  cartList.push(product);
+  setLocalStorage("so-cart", cartList);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
