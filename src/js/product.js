4471c30f517505106828f3566b9bcc5972edee9a
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-01-16 16:48:33 -0700

    Enhance product details page with dynamic rendering and add to cart functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,9 @@
-import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
 
-const productID = getParams("product");
 const dataSource = new ProductData("tents");
+const productID = getParams("product");
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
-
-
-function addProductToCart(product) {
-  const newCartList = getLocalStorage("so-cart");
-  newCartList.push(product);
-  setLocalStorage("so-cart", newCartList);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
