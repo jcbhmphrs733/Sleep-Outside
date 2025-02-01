53210c7d485838c696dc526a4a32487d972271e4
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-02-01 01:02:56 -0700

    trying to fix production server build process by changing capitalization in product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
 import { getParams, loadHeaderFooter } from "./utils.mjs";
-import ProductDetails from "./productDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./productData.mjs";
 
 loadHeaderFooter();
